7c4df655ee2d667bb49c2cd7d8b26bd83c4e5d84
Alexandre <alxp.mendes@gmail.com>
2021-03-05 16:00:50 +0000

    logic

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const playerFactory = (side) => {
 
 const gameBoard = (() => {
     let board = new Array(9);
+    let xPositions = [];
+    let oPositions = [];
+
     const newGame = () => {
+        xPositions = [];
+        oPositions = []
         for (let i = 0; i < 9; i++)
             board[i] = "";
     };
@@ -13,22 +18,40 @@ const gameBoard = (() => {
     }
     const setSquare = (pos, player) => {
         board[pos] = player.side;
+        if (player.side == "X"){
+            xPositions.push(pos);
+        }
+        else {
+            oPositions.push(pos);
+        }
     }
 
     const getBoard = () => {
         return board;
     }
+
+    const getXPositions = () => {
+        return xPositions;
+    }
+
+    const getOPositions = () => {
+        return oPositions;
+    }
+
     return {
         newGame,
         getSquare,
         setSquare,
-        getBoard
+        getBoard,
+        getXPositions,
+        getOPositions
     };
 })();
 
 const game = (() => {
     const gb = gameBoard;
     const board = gb.getBoard();
+    const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]; 
 
     const playFirst = () => {
         const human = playerFactory("X");
@@ -37,9 +60,15 @@ const game = (() => {
         for (let i=0; i<9; i++){
             if (i%2 == 0) {
                 humanPlayer(human);
+                if (checkWinCondition(gb.getXPositions())) {
+                    console.log("You win!")
+                }
             }
             else {
                 computerPlayer(computer);
+                if (checkWinCondition(gb.getOPositions())) {
+                    console.log("You lose!")
+                }
             }
         }
         console.log(board)
@@ -52,17 +81,33 @@ const game = (() => {
         for (let i=0; i<9; i++){
             if (i%2 == 0) {
                 computerPlayer(computer);
+                if (checkWinCondition(gb.getXPositions())) {
+                    console.log("You lose!")
+                }
             }
             else {
                 humanPlayer(human);
+                if (checkWinCondition(gb.getOPositions())) {
+                    console.log("You win!")
+                }
             }
         }
         console.log(board)
     }
 
+    const checkWinCondition = (positions) => {
+        console.log(positions);
+        for (let i = 0; i < 8; i++) {
+            if ((positions.indexOf(winConditions[i][0]) > - 1) && (positions.indexOf(winConditions[i][1]) > - 1) && (positions.indexOf(winConditions[i][2]) > - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     const humanPlayer = (human) => {
         let square = prompt("where?");
-        gb.setSquare(square, human);
+        gb.setSquare(parseInt(square), human);
     }
 
     const computerPlayer = (computer) => {
